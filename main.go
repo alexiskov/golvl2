049af6a78eff24d2ca3bb9deb58e44f798d91de4
alexiskov <alexiskov.rus@gmail.com>
2022-05-31 22:22:14 +0300

    hw1.2

--- main.go
+++ main.go
@@ -3,14 +3,30 @@ package main
 import(
 	"fmt"
 	"os"
-	//"io"
+	"runtime/debug"
 )
+
+type MyWrapErr struct{
+	message string
+	trace string
+}
+
+func NewError (msg string) error{
+	return &MyWrapErr{
+		message: msg,
+		trace: string(debug.Stack()),
+	}
+} 
+
+func (e *MyWrapErr) Error() string {
+	return fmt.Sprintf("--ошибка: %s\n--трассировка:\n%s", e.message, e.trace)
+}
 
 func main(){
 	file, err := os.Open("./nofile")
 	defer func(){
 		if rec:=recover(); rec!=nil{
-			fmt.Println("Восстановлено", rec)
+			fmt.Println("Восстановлено", NewError("file not found"))
 		}
 	}()
 	if err != nil{
