b748e645224e54612d76c68be57e18dc06d16558
alexiskov <alexiskov.rus@gmail.com>
2022-06-14 18:41:26 +0300

    comment

--- main.go
+++ main.go
@@ -1,36 +1,5 @@
 package main
 
-import(
-	"fmt"
-	"os"
-	"runtime/debug"
-)
-
-type MyWrapErr struct{
-	message string
-	trace string
-}
-
-func NewError (msg string) error{
-	return &MyWrapErr{
-		message: msg,
-		trace: string(debug.Stack()),
-	}
-} 
-
-func (e *MyWrapErr) Error() string {
-	return fmt.Sprintf("--ошибка: %s\n--трассировка:\n%s", e.message, e.trace)
-}
-
 func main(){
-	file, err := os.Open("./nofile")
-	defer func(){
-		if rec:=recover(); rec!=nil{
-			fmt.Println("Восстановлено", NewError("file not found"))
-		}
-	}()
-	if err != nil{
-		panic(err)
-	}
-	defer file.Close()
+	//дайте пожалуйста времени чуть, сделаю задание, на работе перегруз, пишу микро-crm для заводика. PR - для того чтобы оставить возможность сдать
 }
